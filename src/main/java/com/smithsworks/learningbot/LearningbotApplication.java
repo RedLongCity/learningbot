00cb7ef05a2cc0a0a30e6204cc024a22a67a7921
Vladimir_Kharchenko <RedLongCity@gmail.com>
2018-11-11 22:36:32 +0200

    changes for wildlife deploying

--- src/main/java/com/smithsworks/learningbot/LearningbotApplication.java
+++ src/main/java/com/smithsworks/learningbot/LearningbotApplication.java
@@ -2,19 +2,12 @@ package com.smithsworks.learningbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 //@EnableMongoRepositories
-public class LearningbotApplication extends SpringBootServletInitializer {
+public class LearningbotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LearningbotApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(LearningbotApplication.class);
     }
 }
