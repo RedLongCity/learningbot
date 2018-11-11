3e89edf05ff276d3dfa65bf890ec6b173d8d8c44
Vladimir_Kharchenko <RedLongCity@gmail.com>
2018-11-11 18:41:50 +0200

    changed to jar - archive

--- src/main/java/com/smithsworks/learningbot/controller/LearningBotController.java
+++ src/main/java/com/smithsworks/learningbot/controller/LearningBotController.java
@@ -2,6 +2,8 @@ package com.smithsworks.learningbot.controller;
 
 import com.pengrad.telegrambot.model.Update;
 import com.smithsworks.learningbot.bot.LearningBot;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,16 +13,20 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class LearningBotController {
 
+    private static final Logger log = LogManager.getLogger();
+
     @Autowired
     private LearningBot learningBot;
 
     @GetMapping("/learningBot")
     public ResponseEntity<String> getHandle() {
+        log.info("GET Method for learningBot");
         return new ResponseEntity<>("Hello Learning Bot!!!", HttpStatus.OK);
     }
 
     @PostMapping("/learningBot")
     public void postHandle(@RequestBody Update update) {
+        log.info("POST Method for learningBot update: \"{}\" value", update.toString());
         learningBot.handle(update);
     }
 }
